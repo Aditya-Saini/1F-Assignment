1c7056f800b7347dacd47686ed0ee5bc2b96f350
Aditya-Saini <aju180601@gmail.com>
2023-04-12 22:43:32 +0530

    FIX: CRUD URL

--- movie/urls.py
+++ movie/urls.py
@@ -4,6 +4,6 @@ from movie.views import MovieListAPIView, CollectionView, CollectionRUDAView
 urlpatterns = [
     path("movies/", MovieListAPIView.as_view(), name="movie"),
     path("collections/", CollectionView.as_view(), name="collections"),
-    path("collections/<uuid:pk>", CollectionRUDAView.as_view(), name="single_collections")
+    path("collections/<uuid:pk>/", CollectionRUDAView.as_view(), name="single_collections")
 
 ]
